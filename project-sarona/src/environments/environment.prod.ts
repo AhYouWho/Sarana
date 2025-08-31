b0dd28e48768bd1cc0bdf514d91b0d9d27aa8236
Oeng_Outeab <103852054+AhYouWho@users.noreply.github.com>
2025-08-31 12:31:04 +0700

    Update for cloud

--- project-sarona/src/environments/environment.prod.ts
+++ project-sarona/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // apiUrl: 'http://backend-api:80/api',
-  apiUrl: 'http://localhost:5000/api',
-  imageUrl: 'http://localhost:5000',
+  apiUrl: 'http://152.42.222.151:5000/api',
+  imageUrl: 'http://152.42.222.151:5000',
   bakongApiUrl: 'https://api-bakong.nbc.gov.kh',
 };
